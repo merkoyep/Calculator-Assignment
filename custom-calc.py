bce80f7939570596cdecc782a313abfbb880c976
merkoyep <81783308+merkoyep@users.noreply.github.com>
2023-08-23 15:15:19 -0700

    Update custom-calc.py

--- custom-calc.py
+++ custom-calc.py
@@ -23,20 +23,40 @@
 
 # Be sure to convert your numeric values to numbers before performing math operations!
 
+intro_msg = "Please enter the weight to the nearest lb of your "
 
 print("""
       Welcome to Marco Airlines!
-      Marco Airlines offers 3 complementary checked bags on each flight, totalling up to 100lbs.
-      This tool will help you calculate your total baggage.
+      Marco Airlines offers 3 complementary checked bags on each flight, totalling up to 100lbs].
+      We charge an additional $2.00CAD per lbs over 100.
+      This tool will help you calculate your total baggage fees.
       """)
 
-bag_1 = int(input("Please enter the weight in lbs of your first bag."))
-bag_2 = int(input("Please enter the weight in lbs of your second bag."))
-bag_3 = int(input("Please enter the weight in lbs of your final bag."))
-sum_of_bags = bag_1 + bag_2 + bag_3
+bag_1 = int(input(f"{intro_msg}first bag."))
+bag_2 = int(input(f"{intro_msg}second bag."))
+bag_3 = int(input(f"{intro_msg}final bag."))
 
+# end of User Input
 
-if sum_of_bags <= 100:
-      print(f"Your bags weigh {sum_of_bags}lbs. You're all set to fly! We hope you enjoy your flight!")
+def bag_total(bag_1, bag_2, bag_3):
+      solution = bag_1 + bag_2 + bag_3
+      return solution
+
+sum_of_bags = bag_total(bag_1, bag_2, bag_3)
+total_overweight = sum_of_bags - 100
+bag_total_msg=f"Your bags weigh {bag_1}lb, {bag_2}lb, and {bag_3}lb. This adds to a total of {sum_of_bags}lbs."
+
+def bag_fee(sum_of_bags):
+      fee = (sum_of_bags - 100) * 2
+      return fee
+
+if total_overweight <= 0:
+      print(f"""
+          {bag_total_msg} 
+            You're all set to fly! We hope you enjoy your flight!""")
 else:
-      print(f"Your bags weigh {sum_of_bags}lbs. Please pay for additional bags or adjust your bags acccordingly. We hope you enjoy your flight!")
+      print(f"""
+            {bag_total_msg} 
+            This is {total_overweight}lb overweight.
+            Marco Airlines charges $2.00CAD for each lb over 100lb. Your total fee due today is ${bag_fee(sum_of_bags)}.00CAD.
+            Please pay this fee online before check-in. Paying during or after check-in will incurr an additional $10.00CAD customer service fee.""")
